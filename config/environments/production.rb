78dc923b5f4c548982604aff7978ce83b541cc8a
hayksaakian <fakest.id.ever@gmail.com>
2012-08-28 19:46:01 -0700

    config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Talktome::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+OPENTOK_AUTH = {"key" => ENV['OPENTOK_KEY'], "secret" => ENV['OPENTOK_SECRET']}
+
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
