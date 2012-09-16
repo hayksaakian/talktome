474d941c5c2eeb433a160a4078b84158b712a32a
hayksaakian <fakest.id.ever@gmail.com>
2012-09-16 14:15:53 -0700

    cleanup pt 2

--- app/models/room.rb
+++
@@ -1,8 +0,0 @@
-class Room
-  include Mongoid::Document
-  include Mongoid::Timestamps
-
-  field :name, :type => String
-  field :sessionId, :type => String
-  field :public, :type => Boolean, :default => false
-end
